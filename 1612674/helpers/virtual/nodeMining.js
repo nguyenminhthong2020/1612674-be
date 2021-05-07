577fd4ccfcff345f4f2d2fe70d15653b53d88179
nguyenminhthong2020 <1612674@student.hcmus.edu.vn>
2021-05-07 16:23:48 +0700

    miner ảo

--- 1612674/helpers/virtual/nodeMining.js
+++ 1612674/helpers/virtual/nodeMining.js
@@ -1,5 +1,6 @@
 
+// miner ảo : chọn ra trong list node m65t node ngẫu nhiên để miner transaction
 module.exports  = () =>{
-    let address = listNode[Math.floor(Math.random() * listNode.length)];
-    blockChain.miningPendingTransactions(address);
+    let randomAddress = listNode[Math.floor(Math.random() * listNode.length)];
+    blockChain.miningPendingTransactions(randomAddress);
 }
